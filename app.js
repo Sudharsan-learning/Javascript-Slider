3e411a59feca7feb3aa8b2efb9bd8d229eb58cf8
sudharsan-learning <sudharsan77anbu@gmail.com>
2022-01-14 19:47:51 +0530

    Intial Commit

--- app.js
+++ app.js
@@ -2,24 +2,23 @@ const slides = document.querySelectorAll(".slide");
 const nextBtn = document.querySelector(".nextBtn");
 const prevBtn = document.querySelector(".prevBtn");
 
-
 slides.forEach(function (slide, index) {
   slide.style.left = `${index * 100}%`;
 });
 
-let counter = 0;
-nextBtn.addEventListener("click", function () {
-  counter++;
-  carousel();
-});
+// let counter = 0;
+// nextBtn.addEventListener("click", function () {
+//   counter++;
+//   carousel();
+// });
 
-prevBtn.addEventListener("click", function () {
-  counter--;
-  carousel();
-});
+// prevBtn.addEventListener("click", function () {
+//   counter--;
+//   carousel();
+// });
 
 function carousel() {
-
+  console.log(counter, "counter");
   if (counter < slides.length - 1) {
     nextBtn.style.display = "block";
   } else {
